80b04fae17d12b5784c7f832f53e5c350f609081
Donna143 <thebestlifeever@gmail.com>
2022-12-18 18:50:21 -0500

    Search test edits

--- src/test/java/pageObjectTests/BaseTest.java
+++ src/test/java/pageObjectTests/BaseTest.java
@@ -1,24 +1,52 @@
 package pageObjectTests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
 
 public class BaseTest {
     protected WebDriver driver;
     protected String url;
+    protected String USERNAME = "thebestlifeever_OH80xE";
+    protected String AUTOMATE_KEY = "zvfa6sUsD8KeXkFhnsC4";
+    protected String BSURL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
 
     @BeforeMethod
-    public void startUp(){
+    public void startUp() throws MalformedURLException {
+
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
         url = "https://www.webstaurantstore.com";
+
+//        MutableCapabilities capabilities = new MutableCapabilities();
+//        capabilities.setCapability("browserName", "Chrome");
+//        HashMap<String, Object> browserstackOptions = new HashMap<String, Object>();
+//        browserstackOptions.put("os", "Windows");
+//        browserstackOptions.put("osVersion", "10");
+//        browserstackOptions.put("browserVersion", "latest");
+//        browserstackOptions.put("projectName", "WebstaurantStore");
+//        browserstackOptions.put("buildName", "Technical Assessment");
+//        browserstackOptions.put("sessionName", "Search");
+//        browserstackOptions.put("local", "false");
+//        browserstackOptions.put("seleniumVersion", "3.141.59");
+//        capabilities.setCapability("bstack:options", browserstackOptions);
+//
+//        driver = new RemoteWebDriver(new URL(BSURL), capabilities);
+//        driver.manage().window().maximize();
+
     }
 
     @AfterMethod
-    public void tearDown(){
-        driver.close();
-    }
+    public void tearDown(){ driver.quit(); }
 }
